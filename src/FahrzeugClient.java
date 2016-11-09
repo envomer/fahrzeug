56ef14605ab5034811e62dafd0b484a83c26c1a4
Omer Mohamed Ali <a1267512@unet.univie.ac.at>
2016-11-09 11:51:04 +0100

    Add FahrzeugClient class

---
+++ src/FahrzeugClient.java
@@ -0,0 +1,11 @@
+public class FahrzeugClient
+{
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+    public String show() {
+        System.out.println("shwoing");
+        return "super";
+    }
+}
